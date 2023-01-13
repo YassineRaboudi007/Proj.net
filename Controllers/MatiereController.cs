5a2014cbc292c82e2b9f8abde32d338700f3106c
hamada-fatma <fatma.hamada@etudiant-isi.utm.tn>
2023-01-13 20:46:47 +0100

    controllers

---
+++ Controllers/MatiereController.cs
@@ -0,0 +1,52 @@
+﻿using Gestion_note.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gestion_note.Controllers
+{
+    public class MatiereController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(MatiereRepository.GetMatieres());
+
+        }
+        [HttpGet]
+        public IActionResult Add(int id)
+        {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add(int id)
+        {
+            ViewBag.ERROR = "";
+            if(id==0)
+            {
+                return View("add_matiere", new Matiere);
+            }
+            else
+            {
+                return View("add_matiere", unitofwork.Matiere.GetByID(id));
+            }
+        }
+         [HttpGet]
+         public IActionResult All()
+            {
+                 return View();
+             }
+          [HttpPost]
+          [Route("/All")]
+          public IActionResult All()
+             {
+                UnitOfWork unitOfWork = new UnitOfWork(AppContext.Instance);
+                List<Matiere> matiere = unitOfWork.Matiere.GetAll().ToList();
+                return View(matiere);
+                }
+
+}
+         
+        
+}
--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gestion_note.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Gestion_note.Controllers
 {
@@ -6,7 +8,40 @@ namespace Gestion_note.Controllers
     {
         public IActionResult Index()
         {
+            //UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            NoteRepository noteRepository = new NoteRepository(universityContext);
+            foreach (String s in noteRepository.GetNote())
+                Debug.WriteLine(s);
+
+            return View(noteRepository.GetNote());
+
+        }
+           
+
+        private readonly INotesRepository _notesRepository;
+
+        public NotesController(INotesRepository notesRepository)
+        {
+            _notesRepository = notesRepository;
+        }
+
+        public ActionResult Add(int id)
+        {
+            ViewBag.EtudiantId = id;
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public ActionResult Add(Note note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(note);
+            }
+
+            _notesRepository.AddNote(note);
+            return RedirectToAction("Index", "Etudiants");
         }
     }
 }
